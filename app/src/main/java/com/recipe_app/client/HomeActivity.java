7ebdf68d6b1288a9ae2ac8f8dcadc2c84f2c61d1
Rincy <rincy@cybraum.net>
2018-04-17 14:29:14 +0530

    first commit

---
+++ app/src/main/java/com/recipe_app/client/HomeActivity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.recipe_app.client;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.recipe_app.R;
+
+/**
+ * This Activity class defines the home screen for the recipe app.
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Handler h=new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent in=new Intent(HomeActivity.this,RecipeActivity.class);
+                startActivity(in);
+            }
+        },3000);
+
+    }
+}
